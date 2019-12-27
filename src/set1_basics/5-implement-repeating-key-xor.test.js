232ba7bde5d829d566220dd4e775a907d26ee63e
Nail Shaikhinurov <n.shaikhinurov@gmail.com>
2019-12-27 16:43:58 +0500

    solved 5

---
+++ src/set1_basics/5-implement-repeating-key-xor.test.js
@@ -0,0 +1,16 @@
+// https://cryptopals.com/sets/1/challenges/3
+import { repeatingXor } from './helpers'
+
+describe('Implement repeating-key XOR', () => {
+	test('should encrypt, using repeating-key XOR', () => {
+		const keyBuf = Buffer.from('ICE', 'utf8')
+		const plainTextBuf1 = Buffer.from(
+			"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal",
+			'utf8'
+		)
+
+		expect(repeatingXor(keyBuf, plainTextBuf1).toString('hex')).toEqual(
+			'0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
+		)
+	})
+})
