fb3a9cbb17e35310b1df5e5c26887b803119a439
Nail Shaikhinurov <n.shaikhinurov@gmail.com>
2020-01-08 18:31:46 +0500

    implemented ctr mode

--- src/set1_basics/courseraChallenge-week2-programming-assignment.js
+++ src/set1_basics/courseraChallenge-week2-programming-assignment.js
@@ -1,3 +1,4 @@
+/* global BigInt */
 import * as R from 'ramda'
 import crypto from 'crypto'
 import { xor } from './helpers'
@@ -40,6 +41,40 @@ const decryptInCbcMode = (key, ciphertextBuffer) => {
 	return decryptionWithPaddingRemoved
 }
 
+const decryptInCtrMode = (key, ciphertextBuffer) => {
+	const bigIntToBuffer = bigInt =>
+		Buffer.from(bigInt.toString(16).padStart(32, '0'), 'hex')
+	const bufferToBigInt = buffer => BigInt(`0x${buffer.toString('hex')}`)
+
+	const iv = R.slice(0, 16)(ciphertextBuffer)
+	const cipherBlocks = R.pipe(
+		R.slice(16, Infinity),
+		R.splitEvery(16),
+		R.map(Buffer.from),
+	)(ciphertextBuffer)
+	// console.log('TCL: decryptInCtrMode -> cipherBlocks', cipherBlocks)
+
+	const decryption = R.addIndex(R.reduce)((decryption, cipherBlock, index) => {
+		const cipher = crypto.createCipheriv('AES-128-ECB', key, null)
+		cipher.setAutoPadding(false) // When using block encryption algorithms, the Cipher class will automatically add padding to the input data to the appropriate block size. To disable the default padding call cipher.setAutoPadding(false).
+
+		const iv_i = bigIntToBuffer(bufferToBigInt(iv) + BigInt(index))
+		// console.log('TCL: decryptInCtrMode -> iv_i', iv_i)
+
+		const nextDecryptedBlock = xor(
+			cipherBlock,
+			Buffer.concat([cipher.update(iv_i), cipher.final()]).slice(
+				0,
+				cipherBlock.length,
+			),
+		)
+
+		return Buffer.concat([decryption, nextDecryptedBlock])
+	}, Buffer.alloc(0))(cipherBlocks)
+
+	return decryption
+}
+
 const cbcEncryptions = [
 	'140b41b22a29beb4061bda66b6747e14',
 	'4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81',
@@ -64,10 +99,10 @@ R.pipe(
 	console.log,
 )(cbcEncryptions)
 
-// R.pipe(
-// 	R.map(hex => Buffer.from(hex, 'hex')),
-// 	R.splitEvery(2),
-// 	R.map(R.apply(decryptInCtrMode)),
-// 	R.map(buf => buf.toString('utf8')),
-// 	console.log,
-// )(ctrEncryptions)
+R.pipe(
+	R.map(hex => Buffer.from(hex, 'hex')),
+	R.splitEvery(2),
+	R.map(R.apply(decryptInCtrMode)),
+	R.map(buf => buf.toString('utf8')),
+	console.log,
+)(ctrEncryptions)
